a5343fb8a9ac4989d4bc3a2b71b87bf2a5f45722
vu <duyvujarvan4@gmail.com>
2021-11-17 21:56:23 +0700

    fixed database

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose")
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/instagram", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    await mongoose.connect(
+      "mongodb+srv://nduyvu:Vu15112000@instagram-clone.agzar.mongodb.net/instagram?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     console.log("connected")
   } catch (error) {
     console.log("failed")
